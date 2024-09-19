4d145ce1a6087acad4af6d76eaa06a54eb1b3b0e
Garik <32434637+Gathrik@users.noreply.github.com>
2024-09-19 11:30:26 +0200

    npm update

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'assets.aceternity.com',
+                port: '',
+                pathname: '/demos/**',
+            },
+        ],
+    },
+};
 
 export default withSentryConfig(nextConfig, {
 // For all available options, see:
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import {AuroraBackground} from "@/components/ui/aurora-background";
+import ExpandableCardDemo from "@/components/blocks/expandable-card-demo-standard";
+
 export default function Home() {
   return (
       <main className="relative mx-auto flex flex-col items-center justify-center overflow-clip px-5 bg-black sm:px-10">
-          <div className="w-full max-w-7xl"></div>
+          <div className="w-full">
+              <AuroraBackground className="z-0" showRadialGradient={true}>
+                  <h1 className="text-3xl font-bold text-black">Connect your Spotify</h1>
+              </AuroraBackground>
+          </div>
       </main>
   );
 }

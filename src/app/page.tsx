9be8c48cfa6acb13fec09c6d87049ef43846b4e4
Garik <32434637+Gathrik@users.noreply.github.com>
2024-09-21 02:32:13 +0200

    Refactor Home component and tweak placeholders input.
    Commented out redirect logic in Home component and updated title from "Spotify" to "Listify." Also, adjusted dependencies for useEffect in placeholders-and-vanish-input.tsx to include `handleVisibilityChange` and `startAnimation`.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,18 +5,18 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-    const router = useRouter();
-
-    useEffect(() => {
-        const accessToken = localStorage.getItem('spotify_access_token');
-        if (accessToken) {
-            router.push("/dashboard");
-        }
-    }, [router]);
+    // const router = useRouter();
+    //
+    // // useEffect(() => {
+    // //     const accessToken = localStorage.getItem('spotify_access_token');
+    // //     if (accessToken) {
+    // //         router.push("/dashboard");
+    // //     }
+    // // }, [router]);
 
     return (
         <main className="my-24 text-center">
-            <h1 className="text-2xl">Spotify</h1>
+            <h1 className="text-2xl">Listify</h1>
             <div className="flex gap-2 justify-center mt-12">
                 <Link
                     className="inline-block py-3 px-6 rounded-md font-bold text-black bg-green-400"
